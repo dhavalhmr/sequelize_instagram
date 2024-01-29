6aa3592df09f127dd677fbda7c77bb9c1375d2b4
Dhaval Jethava <dj.hmrtech@gmail.com>
2024-01-29 12:44:01 +0530

    formatted

--- config/index.ts
+++ config/index.ts
@@ -8,8 +8,15 @@ type ProcessEnv = {
   SESSION_SECRET?: string;
 };
 
-const { DB_NAME, DB_USERNAME, DB_PASSWORD, HOST, PORT, DIALECT,SESSION_SECRET } =
-  process?.env as ProcessEnv;
+const {
+  DB_NAME,
+  DB_USERNAME,
+  DB_PASSWORD,
+  HOST,
+  PORT,
+  DIALECT,
+  SESSION_SECRET,
+} = process?.env as ProcessEnv;
 
 export const config = {
   dbCredential: {
